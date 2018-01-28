af5b0985723eb51ac9841fa9a57f6f7318749021
duanyuwen <duanyuwen@duanyuwendeMacBook-Pro.local>
2018-01-28 21:22:28 +0800

    add lodash

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // html-webpack-inject-attributes-plugin
+var assign = require('lodash.assign');
+var foreach = require('lodash.foreach');
 
 function MyPlugin(options) {
     this.options = options;
@@ -7,30 +9,27 @@ function MyPlugin(options) {
 MyPlugin.prototype.apply = function (compiler) {
     var self = this;
     compiler.plugin('compilation', function (compilation) {
-        compilation.plugin('html-webpack-plugin-alter-asset-tags', function (htmlPluginData, callback) {
-            var options = Object.assign({}, self.options, htmlPluginData.plugin.options.attributes || {});
-            if (options && typeof options === 'object') {
-                for (var key in options) {
-                    var val = options[key];
-                    if (typeof val !== 'string' && typeof val !== 'function') {
-                        return;
-                    }
-                    for (var headI = htmlPluginData.head.length - 1; headI >= 0; headI--) {
-                        var value = val;
-                        if (typeof val === 'function') {
-                            value = val(htmlPluginData.head[headI], compilation);
-                        }
-                        htmlPluginData.head[headI].attributes[key] = value;
-                    }
-                    for (var bodyI = htmlPluginData.body.length - 1; bodyI >= 0; bodyI--) {
-                        var value = val;
-                        if (typeof val === 'function') {
-                            value = val(htmlPluginData.body[bodyI], compilation);
-                        }
-                        htmlPluginData.body[bodyI].attributes[key] = value;
-                    }
-                }
+        function addAttr(tags, key, val) {
+            if (!tags || !tags.length) {
+                return;
             }
+            forEach(tags, function (tag, index) {
+                var value = val;
+                if (typeof val === 'function') {
+                    value = val(tag, compilation, index);
+                }
+                tag.attributes[key] = value;
+            });
+        }
+        compilation.plugin('html-webpack-plugin-alter-asset-tags', function (htmlPluginData, callback) {
+            var options = assign({}, self.options, htmlPluginData.plugin.options && htmlPluginData.plugin.options.attributes);
+            forEach(options, function(val, key) {
+                if (typeof val !== 'string' && typeof val !== 'function') {
+                    return;
+                }
+                addAttr(htmlPluginData.head, key, val);
+                addAttr(htmlPluginData.body, key, val);
+            });
             callback(null, htmlPluginData);
         });
     });
